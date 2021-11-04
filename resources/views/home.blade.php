212700b1007b4a6ef0aacfa184bb9c64f0931a0e
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-11-04 15:13:23 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello world, my name is {{$nome}}</h1>
+    <ul>
+        <li><a href="/pagina-1">Pagina 1</a></li>
+        <li><a href="/pagina-2">Pagina 2</a></li>
+        <li><a href="/pagina-3">Pagina 3</a></li>
+    </ul>
+</body>
+</html>
